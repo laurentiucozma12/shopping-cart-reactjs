60ee6de2b6da447729cb625f297ea80cf7b328c5
Lau <laurentiucozma12@gmail.com>
2024-03-03 13:05:22 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-500 font-bold text-7xl">Hi</h1>
+      <h1 className="text-red-500 font-bold text-7xl text-center">Hi</h1>
     </div>
   );
 }
