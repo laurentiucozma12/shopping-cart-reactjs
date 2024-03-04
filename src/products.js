1e5dadb4fbce4888b996e3bb482326b5f9dd0397
Lau <laurentiucozma12@gmail.com>
2024-03-04 21:52:02 +0200

    products

--- src/products.js
+++ src/products.js
@@ -3,57 +3,73 @@ import product2 from './assets/2.png';
 import product3 from './assets/3.png';
 import product4 from './assets/4.png';
 import product5 from './assets/5.png';
-import product6 from './assets/6.webp';
-import product7 from './assets/7.webp';
-import product8 from './assets/8.webp';
+import product6 from './assets/6.png';
+import product7 from './assets/7.png';
+import product8 from './assets/8.png';
+import product9 from './assets/9.png';
 
 export const PRODUCTS = [
   {
     id: 1,
-    productName: 'IPhone',
-    price: 999.0,
+    productName: 'ROG Flow X13 Gaming Laptop (2023) GV302',
+    price: 2299.99,
     productImage: product1,
+    link: 'https://rog.asus.com/us/laptops/rog-flow/rog-flow-x13-2023-series/',
   },
   {
     id: 2,
-    productName: 'Macbook Pro 2022 (M1)',
-    price: 1999.0,
+    productName: 'ROG Flow X16 Gaming Laptop (2023) GV601',
+    price: 2299.99,
     productImage: product2,
+    link: 'https://rog.asus.com/us/laptops/rog-flow/rog-flow-x16-2023-series/',
   },
   {
     id: 3,
-    productName: 'Cannon M50 Camera',
-    price: 699.0,
+    productName: '2021 ROG Flow X13',
+    price: 1699.99,
     productImage: product3,
+    link: 'https://rog.asus.com/us/laptops/rog-flow/2021-rog-flow-x13-series/',
   },
   {
     id: 4,
-    productName: 'WLS Van Gogh Denim Jacket',
-    price: 228.0,
+    productName: 'ROG Zephyrus G14 (2022)',
+    price: 999.0,
     productImage: product4,
+    link: 'https://rog.asus.com/us/laptops/rog-zephyrus/rog-zephyrus-g14-2022-series/',
   },
   {
     id: 5,
-    productName: 'LED Light Strips',
-    price: 19.99,
+    productName: 'ROG Zephyrus G14 (2024) GA403',
+    price: 2199.99,
     productImage: product5,
+    link: 'https://rog.asus.com/us/laptops/rog-zephyrus/rog-zephyrus-g14-2024/',
   },
   {
     id: 6,
-    productName: 'SPECTRUM LS TEE',
-    price: 68.0,
+    productName: 'ROG Flow Z13 (2023) GZ301',
+    price: 1749.99,
     productImage: product6,
+    link: 'https://rog.asus.com/us/laptops/rog-flow/rog-flow-z13-2023-series/',
   },
   {
     id: 7,
-    productName: 'AUTO SERVICE SHIRT by GOLF WANG',
-    price: 120.0,
+    productName: '2021 ROG Zephyrus S17',
+    price: 3699.99,
     productImage: product7,
+    link: 'https://rog.asus.com/us/laptops/rog-zephyrus/2021-rog-zephyrus-s17-series/',
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
-    price: 40.0,
+    productName: 'ROG Strix G17 Gaming Laptop (2023) G713',
+    price: 1899.0,
     productImage: product8,
+    link: 'https://rog.asus.com/us/laptops/rog-strix/rog-strix-g17-2023-series/',
+  },
+  {
+    id: 9,
+    productName: 'ROG Strix SCAR 18 (2024) G834',
+    price: 2999.99,
+    productImage: product9,
+    link: 'https://rog.asus.com/us/laptops/rog-strix/rog-strix-scar-18-2024/',
   },
 ];
