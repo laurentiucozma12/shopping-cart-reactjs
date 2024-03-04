169ba77b770bb71c42c5a9f93590fc3220ef40a4
Lau <laurentiucozma12@gmail.com>
2024-03-04 12:57:21 +0200

    update

--- src/pages/cart/CartItem.jsx
+++ src/pages/cart/CartItem.jsx
@@ -1,3 +1,39 @@
-export default function CartItem() {
-  return <div></div>;
+import { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { ShopContext } from '../../context/ShopContext';
+
+export default function CartItem(props) {
+  const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
+    useContext(ShopContext);
+
+  return (
+    <div className="m-8 flex h-[250px] w-[700px] items-center rounded-3xl shadow-lg shadow-black">
+      <img src={productImage} alt={productName} className="w-[200px]" />
+      <div className="w-full text-3xl">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p>${price}</p>
+        <div className="countHandle">
+          <button onClick={() => removeFromCart(id)}> - </button>
+          <input
+            value={cartItems[id]}
+            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+            className="mx-3 w-20 text-center text-2xl font-bold"
+          />
+          <button onClick={() => addToCart(id)}> + </button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+CartItem.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    productName: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    productImage: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,16 +1,20 @@
+import { useContext } from 'react';
 import { PRODUCTS } from '../../products.js';
 import { ShopContext } from '../../context/ShopContext';
+import CartItem from './CartItem.jsx';
 
 function Cart() {
+  const { cartItems } = useContext(ShopContext);
+
   return (
     <div>
-      <div className="cart">
-        <h1>Your Cart Items</h1>
+      <div className="dlex-col flex items-center justify-center">
+        <h1 className="text-5xl font-bold">Your Cart Items</h1>
       </div>
-      <div className="cartItems">
+      <div className="flex flex-col items-center justify-center">
         {PRODUCTS.map((product) => {
           if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />;
+            return <CartItem key={product.id} data={product} />;
           }
         })}
       </div>
